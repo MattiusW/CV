78457ee81b5e5779966d40b1bdd54d9c6e817f66
Mateusz Walczyk <mattiusw@gmail.com>
2022-11-07 17:50:39 +0100

    Instrukcje warunkowe

---
+++ C++/instrukcjeWarunkowe.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a;
+    int b;
+    cout << "Podaj liczbe a" << endl;
+    cin >> a;
+    cout << "Podaj liczbę b" << endl;
+    cin >> b;
+
+    if (a < b)
+         cout << a << " jest mniejsze od " << b << endl;
+    else if (a == b)
+    {
+         cout << a << " jest równe " << b << endl;
+	 cout << "a to jest blok ";
+	 cout << "instrukcji warunkowej" << endl;
+    }
+    else
+	 cout << a << " jest większe od " << b << endl;
+
+}
