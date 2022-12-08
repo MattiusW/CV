9432fa51b866f9c18facee89bd74d5a019470e05
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-08 12:23:49 +0100

    Wprowadzanie danych przez uzytkownika

---
+++ C/wejscie.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{   
+    char imie[20];
+    printf("Podaj swoje imie:\n");
+    scanf("%s", imie);  //wprowadzenie danych przez uzytkownika
+    printf("Czesc %s\n", imie);
+    return 0;
+}
