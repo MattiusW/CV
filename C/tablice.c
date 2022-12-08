f9bf4d787e3f6e707dd0235c5418889a425feb85
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-08 11:23:08 +0100

    Tablice

---
+++ C/tablice.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int temp[3];   //tablica
+int tab[5] = {7, 8, 9, 10, 11};
+char imie[]= "Mateusz amator jezyka C"; //zapisywanie stringa za pomoca tablic
+
+int i = 0;
+int j = 0; 
+int k = 5; 
+int proba = 58;  
+
+int main()
+{  
+    printf("%s\n", imie); //rzutowanie stringa '%s' za pomoca printf 
+    printf("%c\n", proba); //wyswietlenie znaku z tabeli ASCII
+    temp[0] = 10;
+    temp[1] = 15;
+    temp[2] = 20;
+   
+    for(i = 0; i < 2; i++)
+    {
+	printf("%d\n", temp[i]);
+    }
+    
+    while(j < 3)
+    {
+	printf("%d\n", temp[j]);
+        j++;
+    }
+    
+    while(k--)
+    {
+        printf("%d\n", tab[k]); 
+    }
+
+    return 0;
+}
