2cc70675b85ea1bfec8ef72987aeb9d173daf4d2
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-19 21:15:16 +0100

    Sprawdzanie czy litera jest w slowie

--- C#/Projects_VS/ConsoleWisielec/ConsoleWisielec/Wisielec.cs
+++ C#/Projects_VS/ConsoleWisielec/ConsoleWisielec/Wisielec.cs
@@ -12,11 +12,11 @@ namespace ConsoleWisielec
         static void Main(string[] args)
         {
             Console.WriteLine("Zagrajmy w wisielca");
-            string[] lista = { "kot", "pies", "panda", "krowa" , "lew", "tygrys", "papuga", "krokodyl", "kameleon"};
+            string[] lista = { "kot", "pies", "panda", "krowa", "lew", "tygrys", "papuga", "krokodyl", "kameleon" };
             Random rnd = new Random();
             int los = rnd.Next(0, lista.Length);
             string x = lista[los];
-            //Console.WriteLine(x);
+            //Console.WriteLine(x);     //sprawdzanie co wylosowalo
             var rozdzielone_slowo = x.ToCharArray();
             gra(rozdzielone_slowo);
         }
@@ -24,27 +24,35 @@ namespace ConsoleWisielec
         {
             Console.WriteLine("Zgadnij o jakim zwierzeciu mysle zaczyna sie na: " + slowo[0] + " konczy sie na: " + slowo[slowo.Length - 1]); //Wyswietlanie ostatniego elementu
             int szanse = 3;
-            
-            for (szanse = 3; szanse > 0; szanse--)
+
+            while (szanse > 0)
             {
                 Console.WriteLine("Masz: " + szanse + " szanse");
                 string caleSlowo = new string(slowo);
                 var zgadywane = Console.ReadLine();
+                bool litera = caleSlowo.Contains(zgadywane);
 
                 if (String.Equals(zgadywane, caleSlowo))
                 {
                     Console.WriteLine("Dobrze to " + caleSlowo + " wygrales!");
                     break;
                 }
+                else if(litera)
+                {
+                    if (true)
+                        Console.WriteLine("Dobrze, ta litera jest w slowie");
+                }
                 else
                 {
                     Console.WriteLine("To nie to sprobuj ponownie");
+                    szanse--;
                     continue;
                 }
             }
-            
+
             if (szanse == 0)
-                Console.WriteLine("Przegrałeś wiszisz na szubienicy!");  
+                Console.WriteLine("Przegrałeś wiszisz na szubienicy!");
         }
     }
+
 }
