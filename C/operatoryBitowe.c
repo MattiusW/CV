fedeb67d7eaf80cedd09076fce7edef22bb7bece
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-19 11:31:23 +0100

    Cwiczenia na operatorach bitowych

--- C/operatoryBitowe.c
+++ C/operatoryBitowe.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    //printf("Wynik: %d\n", 45 << 7); // przesuniecie liczby binarnej o dana liczbe miejsc 
-    //printf("Wynik: %d\n", 36 >> 2); // przesuniecie liczby binarnej o dana liczbe miejsce 
-    //printf("Wynik: %d\n", 72 & 54);
-    //printf("Wynik: %d\n", 84 | 25);
-    //printf("Wynik: %d\n", 27 ^ 17);
-    //printf("%d\n", 20 % 7);
+    printf("Wynik: %d\n", 45 << 7); // przesuniecie liczby binarnej o dana liczbe miejsc 
+    printf("Wynik: %d\n", 36 >> 2); // przesuniecie liczby binarnej o dana liczbe miejsce 
+    printf("Wynik: %d\n", 72 & 54);
+    printf("Wynik: %d\n", 84 | 25);
+    printf("Wynik: %d\n", 27 ^ 17);
+    printf("%d\n", 20 % 7);
     return 0;
 }
 
