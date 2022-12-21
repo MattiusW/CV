6c817e78e49cb5bad52e5a39468b931503e07a9f
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-21 21:27:07 +0100

    Other way to add

---
+++ C#/Projects_VS/ConsoleAdd/ConsoleAdd/NewAddWay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAdd
+{
+    class NewAddWay
+    {
+        static void Main(String[] args)
+        {
+            var table = Enumerable.Range(0, 5)
+            .Select(i =>
+            {
+                Console.WriteLine("Get value");
+                return int.Parse(Console.ReadLine());
+            })
+            .ToList();
+            Console.WriteLine("Min value: " + table.Min());
+        }
+    }
+}
