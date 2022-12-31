63160f47ec60e98fda71377cbe8965d3e423cdfb
Mateusz Walczyk <mattiusw@gmail.com>
2022-12-31 08:38:22 +0100

    Zabezpieczenie przed bialymi znakami

--- BASH/if.sh
+++ BASH/if.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 name="Matthew"; #Bash jest wrazliwe na biale znaki!
+echo "Podaj imie: ";
+read nameOne;
 
-if [ $name = "Matthew" ] #Spacja po if jest potrzebna oraz nawiasy kwadratowe jak i spacja w nawiasach
+if [ "$nameOne" = "Matthew" ] #Spacja po if jest potrzebna oraz nawiasy kwadratowe jak i spacja w nawiasach
 then  #Jezeli warunek sie zgadza wtedy "then"
    echo "Name is correct";
-elif [ $name = "Elizabeth" ];
+elif [ "$nameOne" = "Elizabeth" ]; #Opakowanie zmiennej w apostrofy pozwala na wprowadzanie bialch znakow jako ciagu
 then
    echo "Name is Elizabeth";
 else
    echo "I don't remeber you";
 fi #konczymy odwrotnoscia if
+echo $nameOne;
 echo $name;
 
 
